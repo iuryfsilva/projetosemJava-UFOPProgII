eb9499f4aaa2f5c11a9c9b748ffc65c2a6d150fc
Iury da Fonsêca Silva <iury.silva@aluno.ufop.edu.br>
2020-10-19 16:44:30 -0300

    Criei novos projetos para os projetos que náo estavam funcionando corretamente

--- FirmaEmpregado/src/firma/Empregado.java
+++ FirmaEmpregado/src/firma/Empregado.java
@@ -7,31 +7,28 @@ public class Empregado {
 	private double salarioMensal;
 	
 	public Empregado(String nome, String sobrenome, double salarioMensal) {
-		this.nome = nome;
-		this.sobrenome = sobrenome;
-		this.salarioMensal = salarioMensal;
+		this.setNome(nome);
+		this.setSobrenome(sobrenome);
+		this.setSalarioMensal(salarioMensal);
 	}
 	
 	public String getNome() {
-		return nome;
+		return this.nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 	
 	public String getSobrenome() {
-		return sobrenome;
+		return this.sobrenome;
 	}
-	
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
 	}
 	
 	public double getSalarioMensal() {
-		return salarioMensal;
-	}
-	
+		return this.salarioMensal;
+	}	
 	public void setSalarioMensal(double salarioMensal) {
 		if(salarioMensal < 0.0){
 			this.salarioMensal = 0.0;
